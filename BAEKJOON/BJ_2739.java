bf3366ceb480832def4279ec13755f9c8b9a9524
Yermi <105793579+kycasdzxc@users.noreply.github.com>
2022-07-08 16:42:39 +0900

    2739번: 구구단

---
+++ BAEKJOON/BJ_2739.java
@@ -0,0 +1,18 @@
+package springTest.com.crizen.controller;
+
+import java.util.Scanner;
+
+public class BJ_2739 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = Integer.parseInt(sc.nextLine());
+		
+		if(input >= 2 && input <= 9) {
+			for(int i = 1 ; i <= 9 ; i++) {
+				System.out.println(input + " * " + i +" = " + input*i);
+			}
+		}
+		
+		sc.close();
+	}
+}
