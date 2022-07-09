9e49bc9f2f4bbc32213911a30c8e143fe75429c4
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-09 22:14:40 +0900

    2742번: 기찍 N

---
+++ BAEKJOON/BOJ_2742.java
@@ -0,0 +1,16 @@
+package bj;
+
+import java.util.Scanner;
+
+public class BOJ_2742 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int input = Integer.parseInt(sc.nextLine());
+		
+		for(int i = 0 ; i < input ; i++) {
+				System.out.println(input - i);
+		}
+		sc.close();
+	}
+}
