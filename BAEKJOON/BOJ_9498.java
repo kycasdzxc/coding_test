a5f66753e69ab39f028b3a63e35023a689383d32
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-09 23:02:17 +0900

    9498번: 시험 성적

--- BAEKJOON/BOJ_9498.java
+++ BAEKJOON/BOJ_9498.java
@@ -1,8 +1,8 @@
-package springTest.com.crizen.controller;
+package boj;
 
 import java.util.Scanner;
 
-public class BJ_9498 {
+public class BOJ_9498 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int input = Integer.parseInt(sc.nextLine());
