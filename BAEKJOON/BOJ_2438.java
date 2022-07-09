2552fcbbca25459a3799ed33d50e3f65fd2edde4
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-09 22:03:16 +0900

    2438번: 별 찍기 - 1

---
+++ BAEKJOON/BOJ_2438.java
@@ -0,0 +1,19 @@
+package bj;
+
+import java.util.Scanner;
+
+public class BOJ_2438 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int input = Integer.parseInt(sc.nextLine());
+		
+		for(int i = 1 ; i <= input ; i++) {
+			for(int j = 0 ; j < i ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
