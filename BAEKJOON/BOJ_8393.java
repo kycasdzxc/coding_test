bb2b842301211453aa933a2c26444bf8925bdc1b
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-09 23:02:12 +0900

    8393번: 합

--- BAEKJOON/BOJ_8393.java
+++ BAEKJOON/BOJ_8393.java
@@ -1,8 +1,8 @@
-package springTest.com.crizen.controller;
+package boj;
 
 import java.util.Scanner;
 
-public class BJ_8393 {
+public class BOJ_8393 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
