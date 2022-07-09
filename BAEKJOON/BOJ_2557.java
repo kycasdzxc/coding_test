e4f4d5e27c7cceafe124d66be6ff95118a2312a0
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-09 23:01:01 +0900

    2557번: Hello World

--- BAEKJOON/BOJ_2557.java
+++ BAEKJOON/BOJ_2557.java
@@ -1,6 +1,6 @@
-package springTest.com.crizen.controller;
+package boj;
 
-public class BJ_2557 {
+public class BOJ_2557 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
