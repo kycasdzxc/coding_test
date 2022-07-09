ca07ca1b55a37d8fb7d8f71250f1284bc29a383f
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-09 23:00:44 +0900

    2480번: 주사위 세개

--- BAEKJOON/BOJ_2480.java
+++ BAEKJOON/BOJ_2480.java
@@ -1,8 +1,8 @@
-package springTest.com.crizen.controller;
+package boj;
 
 import java.util.Scanner;
 
-public class BJ_2480 {
+public class BOJ_2480 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
