ebe51456f5eea8400f77e5227ab8ec42113bbd5d
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-11 00:24:05 +0900

    1157번: 단어 공부

--- BAEKJOON/BOJ_1157.java
+++ BAEKJOON/BOJ_1157.java
@@ -3,7 +3,7 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+//import java.util.Arrays;
 
 public class BOJ_1157 {
 	public static void main(String[] args) throws IOException {
