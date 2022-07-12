575be69789c31e33c2628e8e8fafbeaa0bf084dc
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-12 21:32:31 +0900

    10171번: 고양이

---
+++ BAEKJOON/BOJ_10171.java
@@ -0,0 +1,12 @@
+package boj;
+
+public class BOJ_10171 {
+	public static void main(String[] args) {
+		System.out.println(
+				"\\    /\\\n" + 
+				" )  ( ')\n" + 
+				"(  /  )\n" + 
+				" \\(__)|"
+		);
+	}
+}
