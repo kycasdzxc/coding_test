84ed7df16256c43778a1191e657c10297469e49c
Yermi <105793579+kycasdzxc@users.noreply.github.com>
2022-07-13 09:13:31 +0900

    백준(BOJ) - 20220713

---
+++ BAEKJOON/BOJ_11654.java
@@ -0,0 +1,15 @@
+package springTest.com.crizen.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11654 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int x = br.readLine().charAt(0);
+		
+		System.out.println(x);
+	}
+}
