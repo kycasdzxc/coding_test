a2642255a926f8f66a867be4128bfaa5b2180f95
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-19 00:02:31 +0900

    백준(BOJ) - 20220718

--- BAEKJOON/BOJ_2741.java
+++ BAEKJOON/BOJ_2741.java
@@ -1,7 +1,17 @@
 package boj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BOJ_2741 {
-	public static void main(String[] args) {
-		System.out.printf("59\nkycasdzxc");
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int num = Integer.parseInt(br.readLine());
+		
+		for(int i = 1 ; i <= num ; i++) {
+			System.out.println(i);
+		}
 	}
 }
