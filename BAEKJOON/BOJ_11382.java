4b2cea391c627113e392f244d34b10acda151714
Yermi <105793579+kycasdzxc@users.noreply.github.com>
2022-07-18 09:11:00 +0900

    백준(BOJ) - 20220718

---
+++ BAEKJOON/BOJ_11382.java
@@ -0,0 +1,15 @@
+package springTest.com.crizen.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11382 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		System.out.println(Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken()));
+	}
+}
