d863f65973612323d6ebdd6321c8a359f39152ef
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-22 22:56:36 +0900

    백준(BOJ) - 20220722

---
+++ BAEKJOON/BOJ_2338.java
@@ -0,0 +1,19 @@
+package boj;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ_2338 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+
+		System.out.println(a.add(b));
+		System.out.println(a.subtract(b));
+		System.out.println(a.multiply(b));
+		
+		sc.close();
+	}
+}
