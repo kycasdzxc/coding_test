c75244b3829a339b99dbc16f108d3d6e154de279
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-27 00:28:38 +0900

    백준(BOJ) - 20220726

---
+++ BAEKJOON/BOJ_25314.java
@@ -0,0 +1,23 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_25314 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		while(num > 0) {
+			bw.append("long ");
+			num -= 4;
+		}
+		bw.append("int");
+		bw.flush();
+	}
+}
