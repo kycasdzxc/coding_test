d2c53e0f753c78f23dde8f8defa482defd10d98d
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-07-30 10:44:27 +0900

    백준(BOJ) - 20220730

---
+++ BAEKJOON/BOJ_5337.java
@@ -0,0 +1,10 @@
+package boj;
+
+public class BOJ_5337 {
+	public static void main(String[] args) {
+		System.out.println(
+				".  .   .\n" + 
+				"|  | _ | _. _ ._ _  _\n" + 
+				"|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
