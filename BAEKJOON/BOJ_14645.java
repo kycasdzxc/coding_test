ffe892ae2d44ba75a3c23c0e7e10341644639a0b
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-08-09 22:31:08 +0900

    백준(BOJ) - 20220809

---
+++ BAEKJOON/BOJ_14645.java
@@ -0,0 +1,20 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_14645 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int num = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0 ; i < num ; i++) {
+			br.readLine();
+		}
+		
+		System.out.println("비와이");
+	}
+}
