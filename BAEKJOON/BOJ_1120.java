354fbccf41b56220c39fead5a3b2e0563ac35e51
Yermi <105793579+kycasdzxc@users.noreply.github.com>
2023-08-14 18:11:50 +0900

    백준(BOJ) - 20230814

---
+++ BAEKJOON/BOJ_1120.java
@@ -0,0 +1,30 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1120 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		String a = st.nextToken();
+		String b = st.nextToken();
+		
+		int result = a.length();
+		
+		for(int i = 0 ; i <= b.length() - a.length() ; i++) {
+			int cnt = 0;
+			for(int j = 0 ; j < a.length() ; j++) {
+				if(a.charAt(j) != b.charAt(j + i)) {
+					cnt++;
+				}
+			}
+			result = Math.min(cnt, result);
+		}
+		
+		System.out.println(result);
+	}
+}
